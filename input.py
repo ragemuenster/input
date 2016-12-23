582e1cfddd3f7d4b67b883b03c17b4d8662c7dc2
ragemuenster <kenneth.bicknell15@gmail.com>
2016-12-23 15:30:19 -0500

    Add files via upload

---
+++ input.py
@@ -0,0 +1,19 @@
+form="""
+<form action="http://www.google.com/search">
+  <input name="q">
+  <input type="submit">
+</form>
+"""
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        #self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(form)
+
+class TestHandler(webapp2.RequestHandler):
+    def get(self):
+        q = self.request.get("q")
+        self.response.out.write(q)
+
+app = webapp2.WSGIApplication([('/',MainPage),
+                               ('/testform', TestHandler)],
+                               debug=true)
